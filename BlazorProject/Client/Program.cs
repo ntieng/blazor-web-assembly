4f818bb0450736b692f2f605652c36bc05602f1c
ntieng <noveltieng@gmail.com>
2021-07-13 23:50:15 +0800

    Display employee list in index page

--- BlazorProject/Client/Program.cs
+++ BlazorProject/Client/Program.cs
@@ -1,3 +1,4 @@
+using BlazorProject.Client.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,7 +20,12 @@ namespace BlazorProject.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHttpClient<IEmployeeService, EmployeeService>(client =>
+            {
+                client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+            });
+
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddSyncfusionBlazor();
             await builder.Build().RunAsync();
         }
