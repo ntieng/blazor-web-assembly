5698ad0e530698a52f08d8f8666b2bf37bf7834d
ntieng <noveltieng@gmail.com>
2021-07-14 09:02:16 +0800

    1. Server a. Add sensitive data logging

--- BlazorProject/Server/Startup.cs
+++ BlazorProject/Server/Startup.cs
@@ -22,7 +22,8 @@ namespace BlazorProject.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
+            services.AddDbContext<AppDbContext>(options => 
+                options.EnableSensitiveDataLogging().UseSqlServer(Configuration.GetConnectionString("DBConnection")));
 
             services.AddScoped<IDepartmentRepository, DepartmentRepository>();
             services.AddScoped<IEmployeeRepository, EmployeeRepository>();
